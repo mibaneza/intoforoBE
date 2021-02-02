2c2f6b22865095633cf63484a8f05a068f8e44f2
mibaneza <mibaneza@ucvvirtual.edu.pe>
2021-02-02 18:19:32 -0500

    v.0003

---
+++ src/main/java/com/ard333/springbootwebfluxjjwt/model/Duall.java
@@ -0,0 +1,30 @@
+package com.ard333.springbootwebfluxjjwt.model;
+
+public class Duall {
+    private String dualv1;
+    private String dualv2;
+
+    public Duall() {
+    }
+
+    public Duall(String dualv1, String dualv2) {
+        this.dualv1 = dualv1;
+        this.dualv2 = dualv2;
+    }
+
+    public String getDualv1() {
+        return dualv1;
+    }
+
+    public void setDualv1(String dualv1) {
+        this.dualv1 = dualv1;
+    }
+
+    public String getDualv2() {
+        return dualv2;
+    }
+
+    public void setDualv2(String dualv2) {
+        this.dualv2 = dualv2;
+    }
+}
