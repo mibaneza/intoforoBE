684e216635e3eb9d9a037783cb07cef4c1d85f13
mibaneza <mibaneza@ucvvirtual.edu.pe>
2021-02-22 19:31:15 -0500

    v1.0032

--- src/main/java/com/ard333/springbootwebfluxjjwt/domain/CategoriesDomain.java
+++ src/main/java/com/ard333/springbootwebfluxjjwt/domain/CategoriesDomain.java
@@ -2,12 +2,7 @@ package com.ard333.springbootwebfluxjjwt.domain;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.index.Indexed;
-import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
-
-import java.util.Date;
-import java.util.List;
-import java.util.Set;
 
 @Document(collection = "categories")
 public class CategoriesDomain {
@@ -15,14 +10,14 @@ public class CategoriesDomain {
     @Id
     private String idcategories;
 
-    private String tileCategories;
+    private String tilecategories;
 
     private String description;
 
     @Indexed(unique=true)
     private String linktitle;
 
-    private String linkLogo;
+    private String linklogo;
 
     private String idpost;
 
@@ -30,11 +25,11 @@ public class CategoriesDomain {
 
 
 
-    public CategoriesDomain(String tileCategories, String description, String linktitle, String linkLogo) {
-        this.tileCategories = tileCategories;
+    public CategoriesDomain(String tilecategories, String description, String linktitle, String linkLogo) {
+        this.tilecategories = tilecategories;
         this.description = description;
         this.linktitle = linktitle;
-        this.linkLogo = linkLogo;
+        this.linklogo = linkLogo;
     }
 
 
@@ -56,12 +51,12 @@ public class CategoriesDomain {
         this.idcategories = idcategories;
     }
 
-    public String getTileCategories() {
-        return tileCategories;
+    public String getTilecategories() {
+        return tilecategories;
     }
 
-    public void setTileCategories(String tileCategories) {
-        this.tileCategories = tileCategories;
+    public void setTilecategories(String tilecategories) {
+        this.tilecategories = tilecategories;
     }
 
     public String getDescription() {
@@ -88,12 +83,12 @@ public class CategoriesDomain {
         this.description = description;
     }
 
-    public String getLinkLogo() {
-        return linkLogo;
+    public String getLinklogo() {
+        return linklogo;
     }
 
-    public void setLinkLogo(String linkLogo) {
-        this.linkLogo = linkLogo;
+    public void setLinklogo(String linklogo) {
+        this.linklogo = linklogo;
     }
 
 }
